3d7b6f76fd05f9dfd439ccc52b7d2f72a337bdc6
Nikolai <nikolai.laevskii@yandex.ru>
2018-10-15 00:19:40 +0300

    Исправлена функция рассчета времени

--- libs/date-diff.js
+++ libs/date-diff.js
@@ -1,8 +1,10 @@
-module.exports = (startDate, diff = (Date.now() - startDate)) => [ 3600000, 60000, 1000 ].reduce((ac, cv, ci) => {
+module.exports = function dateDiff (startDate) {
+  let diff = (Date.now() - startDate)
+
+  return [ 3600000, 60000, 1000 ].reduce((ac, cv, ci) => {
     let delta = Math.floor(diff / cv)
-    diff = diff - delta
+    diff = diff - delta * cv
     delta = String(delta)
-    delta = delta.length >= 2 ? delta : '0' + delta
-    ac += String(delta) + (ci === 2 ? '' : ':')
-    return ac
+    return ac += (delta.length >= 2 ? delta : '0' + delta) + (ci === 2 ? '' : ':')
   }, '')
+}
